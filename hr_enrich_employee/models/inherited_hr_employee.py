e943572f169092b3b81587c8f4867a67559e4072
ruhullahil <ruhullahilt@gmail.com>
2025-05-13 17:12:09 +0600

    [ADD] add info in profile view

--- hr_enrich_employee/models/inherited_hr_employee.py
+++ hr_enrich_employee/models/inherited_hr_employee.py
@@ -2,6 +2,8 @@ from dateutil.relativedelta import relativedelta
 from reportlab.graphics.transform import inverse
 
 from odoo import api, fields, models
+from odoo.api import private
+
 
 class HrEmployee(models.Model):
     _name = 'hr.employee'
@@ -25,15 +27,15 @@ class HrEmployee(models.Model):
     identification_type = fields.Selection([('nid','NID'),('birth','Birth Certificate')])
 
     # permanent Address
-    permanent_street = fields.Char(string="Permanent Street", groups="hr.group_hr_user")
-    permanent_street2 = fields.Char(string="Permanent Street2", groups="hr.group_hr_user")
-    permanent_city = fields.Char(string="Permanent City", groups="hr.group_hr_user")
+    permanent_street = fields.Char(string="Permanent Street", groups="base.group_user")
+    permanent_street2 = fields.Char(string="Permanent Street2", groups="base.group_user")
+    permanent_city = fields.Char(string="Permanent City", groups="base.group_user")
     permanent_state_id = fields.Many2one(
         "res.country.state", string="Permanent State",
         domain="[('country_id', '=?', permanent_country_id)]",
-        groups="hr.group_hr_user")
-    permanent_zip = fields.Char(string="Permanent Zip", groups="hr.group_hr_user")
-    permanent_country_id = fields.Many2one("res.country", string="Permanent Country", groups="hr.group_hr_user")
+        groups="base.group_user")
+    permanent_zip = fields.Char(string="Permanent Zip", groups="base.group_user")
+    permanent_country_id = fields.Many2one("res.country", string="Permanent Country", groups="base.group_user")
 
     # reg related info
     reg_submission_date = fields.Date()
@@ -80,6 +82,20 @@ class HrEmployee(models.Model):
             employee.contract_id.contract_type_id = employee.contract_type_id
 
 
+
+    def fetch(self, field_names):
+        if self.browse().has_access('read'):
+            return super().fetch(field_names)
+        # TODO : have to re-write comment
+        # HACK: retrieve publicly available values from hr.employee.public and
+        # copy them to the cache of self; non-public data will be missing from
+        # cache, and interpreted as an access error
+        public_fields = self.env['hr.employee.public']._fields
+        new_fields = [fname for fname in field_names if fname in public_fields]
+        res = super().fetch(new_fields)
+        return res
+
+
 class HrEmployeeBase(models.AbstractModel):
     _inherit = 'hr.employee.base'
 
@@ -94,11 +110,36 @@ class HrEmployeeBase(models.AbstractModel):
         for employee in self:
             employee.parent_id = None
 
+HR_WRITABLE_FIELDS = [
+        'blood_group',
+        'father_name',
+        'mother_name',
+        'permanent_street',
+        'permanent_street2',
+        'permanent_city',
+        'permanent_state_id',
+        'permanent_zip',
+        'permanent_country_id',
+        'tin',
+        'driving_license',
+        'identification_type',
 
+    ]
+
+HR_READABLE_FIELDS = [
+    'job_rank_id',
+    'religion_id',
+    'date_of_joining',
+    'employment_year',
+    'barcode',
+    'relation_with_em_contact',
+]
 
 
 class InheritResUser(models.Model):
     _inherit = 'res.users'
+
+
 
     job_rank_id = fields.Many2one(related='employee_id.rank_id', readonly=False, related_sudo=False)
     religion_id = fields.Many2one(related='employee_id.religion_id', readonly=False, related_sudo=False)
@@ -114,7 +155,20 @@ class InheritResUser(models.Model):
     permanent_zip = fields.Char(string="Permanent Zip", related='employee_id.permanent_zip', readonly=False, related_sudo=False)
     permanent_country_id = fields.Many2one("res.country", string="Permanent Country", related='employee_id.permanent_country_id', readonly=False, related_sudo=False)
     date_of_joining = fields.Date(related='employee_id.date_of_joining', readonly=True, related_sudo=False)
+    tin = fields.Char(related='employee_id.tin', readonly=True, related_sudo=False)
+    driving_license = fields.Char(related='employee_id.driving_license', readonly=True, related_sudo=False)
+    relation_with_em_contact = fields.Char(related='employee_id.relation_with_em_contact', readonly=True, related_sudo=False)
+    identification_type = fields.Selection(related='employee_id.identification_type', readonly=True, related_sudo=False)
     employment_year = fields.Integer(related='employee_id.employment_year')
+    barcode = fields.Char(related='employee_id.barcode')
+
+    @property
+    def SELF_READABLE_FIELDS(self):
+        return super().SELF_READABLE_FIELDS + HR_READABLE_FIELDS + HR_WRITABLE_FIELDS
+
+    @property
+    def SELF_WRITEABLE_FIELDS(self):
+        return super().SELF_WRITEABLE_FIELDS + HR_WRITABLE_FIELDS
 
 
 
